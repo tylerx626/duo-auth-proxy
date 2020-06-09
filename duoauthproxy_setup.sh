8755761a965a5f07d2cb0c0099dc26707f9aa5fe
Tyler <tyler@carlisletech.com>
2020-06-09 12:34:04 -0500

    Create duoauthproxy_setup.sh

---
+++ duoauthproxy_setup.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#This script will download, install, and configure Duo authentication proxy for CentOS 7.x minimal servers
+#Official instructions here https://duo.com/docs/authproxy-reference
+
+
+#install requirements
+yum install -y gcc make libffi-devel perl zlib-devel
+
+
+#download latest Duo version
+if [ test -f duoauthproxy-latest-src.tgz ]
+then
+    echo "File duoauthproxy-latest-src.tgz exists"
+else
+    echo "Downloading latest duoauthproxy-latest-src.tgz file..."
+    wget https://dl.duosecurity.com/duoauthproxy-latest-src.tgz
+fi
+
+#check if install directory exists and mkdir if not
+if [ -d "/opt/duoauthproxy_latest" ] 
+then
+    echo "Directory /opt/duoauthproxy_latest exists." 
+else
+    echo "Error: Directory /opt/duoauthproxy_latest does not exist."
+    mkdir /opt/duoauthproxy_latest
+fi
+
+#extract downloaded tarball and change directory 
+tar zxf duoauthproxy-latest-src.tgz -C /opt/duoauthproxy_latest --strip-components=1
+
+
+#build and install auth proxy
+make -C /opt/duoauthproxy_latest 
+/opt/duoauthproxy_latest/./install
+
+
+#cleanup
+sudo rm duoauthproxy-latest-src.tgz
+
+ 
+#Now test and make sure auth is working.
